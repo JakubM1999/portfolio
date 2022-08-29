4810ce364e965c03c4cd8aa661832daa8b46ac30
JakubM1999 <Gemixe1234@gmail.com>
2022-08-29 11:16:31 +0000

    created selector component file and styled accoring to figma

---
+++ components/selector.tsx
@@ -0,0 +1,61 @@
+import { Button, Container, List, Typography } from "@mui/material";
+import { useState } from "react";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { FC } from "react";
+
+type Props = {
+  items: string[];
+  onSelect: (vaule: string) => void;
+  value: string;
+};
+
+export const Selector = () => {
+  const [selected, setSelected] = useState("");
+
+
+  const handleSelecting = () => {};
+
+  
+  return (
+    <Container>
+      <Container>
+        <Button sx={{ border: "solid 1px black", width: 136, color: "black" }}>
+          <KeyboardArrowUpIcon sx={{ color: "black" }} />
+        </Button>
+      </Container>
+
+      <Container>
+        <li
+            value={selected}
+            onChange={(e) => {
+            const selectedItem = (e.target as HTMLInputElement).value;
+            setSelected(selectedItem);
+          }}
+        >
+          <option value={"RECOMMENDATIONS"}>RECOMMENDATIONS</option>
+          <option value={"CV"}>CV</option>
+          <option selected value={"ABOUT"}>ABOUT</option>
+          <option value={"PROJECTS"}>PROJECTS</option>
+          <option value={"ILLUSTRATIONS"}>ILLUSTRATIONS</option>
+        </li>
+      </Container>
+      
+      <Container>
+        <Button sx={{ border: "solid 1px black", width: 136, color: "black" }}>
+          <KeyboardArrowDownIcon sx={{ color: "black" }} />
+        </Button>
+      </Container>
+      {selected}
+    </Container>
+  );
+};
+
+// const selectItems = [
+  //     {item: 'RECOMMENDATIONS'},
+  //     {item: 'CV'},
+  //     {item: 'ABOUT'},
+  //     {item: 'PROJECTS'},
+  //     {item: 'ILLUSTRATIONS'},
+  // ]
+  // const listItems = selectItems.map((items) => {return <Typography key={items.item}>{items.item}</Typography>})
